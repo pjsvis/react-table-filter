d45e7453abf46be0f7627c325f1769577e83bbfa
Peter John Smith <pjsvis@protonmail.com>
2023-02-03 21:35:26 +0000

    Update 2 files

--- src/main.tsx
+++ src/main.tsx
@@ -31,7 +31,7 @@ import {
 } from "@tanstack/match-sorter-utils";
 
 import { makeData, Person } from "./makeData";
-import { NavButton } from "./nav-button";
+import { Filter } from "./filter";
 import { TableNav } from "./table-nav";
 
 declare module "@tanstack/table-core" {
@@ -251,71 +251,6 @@ function App() {
       
       <TableNav table={table} />
       
-      {/* START TABLE NAV */}
-      {/* <div className="h-2" />
-      <div className="flex items-center gap-2">
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<<"}
-        </button>
-
-        <button
-          className="border rounded p-1"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<"}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {">"}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {">>"}
-        </button>
-        <span className="flex items-center gap-1">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              table.setPageIndex(page);
-            }}
-            className="border p-1 rounded w-16"
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value));
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div> */}
-      {/* END TABLE NAV */}
       <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
       <div>
         <button onClick={() => rerender()}>Force Rerender</button>
@@ -328,82 +263,82 @@ function App() {
   );
 }
 
-function Filter({
-  column,
-  table,
-}: {
-  column: Column<any, unknown>;
-  table: Table<any>;
-}) {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id);
+// function Filter({
+//   column,
+//   table,
+// }: {
+//   column: Column<any, unknown>;
+//   table: Table<any>;
+// }) {
+//   const firstValue = table
+//     .getPreFilteredRowModel()
+//     .flatRows[0]?.getValue(column.id);
 
-  const columnFilterValue = column.getFilterValue();
+//   const columnFilterValue = column.getFilterValue();
 
-  const sortedUniqueValues = React.useMemo(
-    () =>
-      typeof firstValue === "number"
-        ? []
-        : Array.from(column.getFacetedUniqueValues().keys()).sort(),
-    [column.getFacetedUniqueValues()]
-  );
+//   const sortedUniqueValues = React.useMemo(
+//     () =>
+//       typeof firstValue === "number"
+//         ? []
+//         : Array.from(column.getFacetedUniqueValues().keys()).sort(),
+//     [column.getFacetedUniqueValues()]
+//   );
 
-  return typeof firstValue === "number" ? (
-    <div>
-      <div className="flex space-x-2">
-        <DebouncedInput
-          type="number"
-          min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
-          max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
-          value={(columnFilterValue as [number, number])?.[0] ?? ""}
-          onChange={(value) =>
-            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
-          }
-          placeholder={`Min ${
-            column.getFacetedMinMaxValues()?.[0]
-              ? `(${column.getFacetedMinMaxValues()?.[0]})`
-              : ""
-          }`}
-          className="w-24 border shadow rounded"
-        />
-        <DebouncedInput
-          type="number"
-          min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
-          max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
-          value={(columnFilterValue as [number, number])?.[1] ?? ""}
-          onChange={(value) =>
-            column.setFilterValue((old: [number, number]) => [old?.[0], value])
-          }
-          placeholder={`Max ${
-            column.getFacetedMinMaxValues()?.[1]
-              ? `(${column.getFacetedMinMaxValues()?.[1]})`
-              : ""
-          }`}
-          className="w-24 border shadow rounded"
-        />
-      </div>
-      <div className="h-1" />
-    </div>
-  ) : (
-    <>
-      <datalist id={column.id + "list"}>
-        {sortedUniqueValues.slice(0, 5000).map((value: any) => (
-          <option value={value} key={value} />
-        ))}
-      </datalist>
-      <DebouncedInput
-        type="text"
-        value={(columnFilterValue ?? "") as string}
-        onChange={(value) => column.setFilterValue(value)}
-        placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
-        className="w-36 border shadow rounded"
-        list={column.id + "list"}
-      />
-      <div className="h-1" />
-    </>
-  );
-}
+//   return typeof firstValue === "number" ? (
+//     <div>
+//       <div className="flex space-x-2">
+//         <DebouncedInput
+//           type="number"
+//           min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
+//           max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
+//           value={(columnFilterValue as [number, number])?.[0] ?? ""}
+//           onChange={(value) =>
+//             column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+//           }
+//           placeholder={`Min ${
+//             column.getFacetedMinMaxValues()?.[0]
+//               ? `(${column.getFacetedMinMaxValues()?.[0]})`
+//               : ""
+//           }`}
+//           className="w-24 border shadow rounded"
+//         />
+//         <DebouncedInput
+//           type="number"
+//           min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
+//           max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
+//           value={(columnFilterValue as [number, number])?.[1] ?? ""}
+//           onChange={(value) =>
+//             column.setFilterValue((old: [number, number]) => [old?.[0], value])
+//           }
+//           placeholder={`Max ${
+//             column.getFacetedMinMaxValues()?.[1]
+//               ? `(${column.getFacetedMinMaxValues()?.[1]})`
+//               : ""
+//           }`}
+//           className="w-24 border shadow rounded"
+//         />
+//       </div>
+//       <div className="h-1" />
+//     </div>
+//   ) : (
+//     <>
+//       <datalist id={column.id + "list"}>
+//         {sortedUniqueValues.slice(0, 5000).map((value: any) => (
+//           <option value={value} key={value} />
+//         ))}
+//       </datalist>
+//       <DebouncedInput
+//         type="text"
+//         value={(columnFilterValue ?? "") as string}
+//         onChange={(value) => column.setFilterValue(value)}
+//         placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+//         className="w-36 border shadow rounded"
+//         list={column.id + "list"}
+//       />
+//       <div className="h-1" />
+//     </>
+//   );
+// }
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
